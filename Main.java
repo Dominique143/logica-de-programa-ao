feba380287e9904a2085184f3dbfee93bfc5ae77
Dominque <dominique76725@estudante.ifb.edu.br>
2025-11-19 15:56:57 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a, b,soma;
+	    
+	    a = 354;
+	    b = 10;
+	    soma = a + b;
+	    
+		System.out.println("A Soma é "  + soma);
+	}
+}
